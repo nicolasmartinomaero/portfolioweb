a1177f8fcb4255c1a7831d1368bbcdd6b9901dfe
nicolas martino <nicolasmartinomaero@gmail.com>
2022-03-07 12:48:49 -0300

    7marzo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadosComponent } from './componentes/encabezados/encabezados.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { PiepaginaComponent } from './componentes/piepagina/piepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadosComponent,
+    AcercadeComponent,
+    EducacionComponent,
+    PiepaginaComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/componentes/encabezados/encabezados.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EncabezadosComponent } from './encabezados.component';
+
+describe('EncabezadosComponent', () => {
+  let component: EncabezadosComponent;
+  let fixture: ComponentFixture<EncabezadosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EncabezadosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EncabezadosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
